53b1ee8bf04c1f41abe75ee02058d18d36971228
Wes Dencker <wdencker@gmail.com>
2018-12-06 11:12:02 -0500

    Updates

---
+++ precache-manifest.e7a0bec5e735c7240b71b58665279f51.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fe3a21932ddf1fe08cc",
+    "url": "/cs1300-development/static/css/main.f25372ff.chunk.css"
+  },
+  {
+    "revision": "9fe3a21932ddf1fe08cc",
+    "url": "/cs1300-development/static/js/main.9fe3a219.chunk.js"
+  },
+  {
+    "revision": "f906443ee4abbb7aafc9",
+    "url": "/cs1300-development/static/js/1.f906443e.chunk.js"
+  },
+  {
+    "revision": "9ebee343062eecc68a9c",
+    "url": "/cs1300-development/static/js/runtime~main.9ebee343.js"
+  },
+  {
+    "revision": "6356df0274319f46ec36ceda656aca6d",
+    "url": "/cs1300-development/static/media/kabel.6356df02.ttf"
+  },
+  {
+    "revision": "212ded6faf1bfc19bfe1599b613451c0",
+    "url": "/cs1300-development/static/media/Copperplate Gothic Regular.212ded6f.otf"
+  },
+  {
+    "revision": "6901c65d6d291c5e2cbb9b44aaa905f7",
+    "url": "/cs1300-development/static/media/FuturaTEE Regular.6901c65d.ttf"
+  },
+  {
+    "revision": "f66f22c075e166f0be9de46c4f105801",
+    "url": "/cs1300-development/static/media/atlantic.f66f22c0.jpg"
+  },
+  {
+    "revision": "e7371c535efa7c4e47a255087197d83c",
+    "url": "/cs1300-development/static/media/baltic.e7371c53.jpg"
+  },
+  {
+    "revision": "4d85581a5d1efea7324dd10c98c0305e",
+    "url": "/cs1300-development/static/media/boardwalk.4d85581a.jpg"
+  },
+  {
+    "revision": "b0edc20bc95c782cef4e4de711bbdbb8",
+    "url": "/cs1300-development/static/media/charles.b0edc20b.jpg"
+  },
+  {
+    "revision": "b3a88924858c2e67aa3d0dcc9aa63db8",
+    "url": "/cs1300-development/static/media/connecticut.b3a88924.jpg"
+  },
+  {
+    "revision": "859b1ae96fd7ee6a03d6287620de687f",
+    "url": "/cs1300-development/static/media/illinois.859b1ae9.png"
+  },
+  {
+    "revision": "7b2cd9a687914070b6de3791ab784966",
+    "url": "/cs1300-development/static/media/indiana.7b2cd9a6.jpg"
+  },
+  {
+    "revision": "9d6306a2ec5738809cc96ab0c8b89baa",
+    "url": "/cs1300-development/static/media/james.9d6306a2.jpg"
+  },
+  {
+    "revision": "99f363b6aea914471086e8357902c457",
+    "url": "/cs1300-development/static/media/kentucky.99f363b6.jpg"
+  },
+  {
+    "revision": "6300b48a9d0d08687c7f0e723d7ace07",
+    "url": "/cs1300-development/static/media/marvin.6300b48a.jpg"
+  },
+  {
+    "revision": "52ea67bcd434386447e047a31c724381",
+    "url": "/cs1300-development/static/media/mediterranean.52ea67bc.jpg"
+  },
+  {
+    "revision": "5d6e2901a951d07f8cd5c9a20d53bb37",
+    "url": "/cs1300-development/static/media/nc.5d6e2901.jpg"
+  },
+  {
+    "revision": "f8cee32199f882478a222c98e3872fcb",
+    "url": "/cs1300-development/static/media/ny.f8cee321.jpg"
+  },
+  {
+    "revision": "eddb1c59b22cf26c3588c9439f7a159a",
+    "url": "/cs1300-development/static/media/oriental.eddb1c59.jpg"
+  },
+  {
+    "revision": "5c1a9a7a712da17d918c204eacd64973",
+    "url": "/cs1300-development/static/media/pacific.5c1a9a7a.jpg"
+  },
+  {
+    "revision": "fc5869d33b16e5c5bc2c69c5dcbf1d6a",
+    "url": "/cs1300-development/static/media/park.fc5869d3.jpg"
+  },
+  {
+    "revision": "9f4261f76e9587abd100caf8170b4d1c",
+    "url": "/cs1300-development/static/media/pennsylvania.9f4261f7.jpg"
+  },
+  {
+    "revision": "e62c0f07192242a85916c22d23aaff5f",
+    "url": "/cs1300-development/static/media/states.e62c0f07.jpg"
+  },
+  {
+    "revision": "f9e68f3a088022e4c0c58f9b1edb8363",
+    "url": "/cs1300-development/static/media/tennessee.f9e68f3a.jpg"
+  },
+  {
+    "revision": "47703b4669790825efb524e5ccf34d1f",
+    "url": "/cs1300-development/static/media/ventnor.47703b46.jpg"
+  },
+  {
+    "revision": "2f4c96a1cce80a1bafd5ec838a47a8c8",
+    "url": "/cs1300-development/static/media/vermont.2f4c96a1.png"
+  },
+  {
+    "revision": "9cb90f3ab4ec5b99526dfd03529d8e36",
+    "url": "/cs1300-development/static/media/virginia.9cb90f3a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cs1300-development/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b79243cb320a5fdab2df7ae82f36cdba",
+    "url": "/cs1300-development/index.html"
+  }
+];
